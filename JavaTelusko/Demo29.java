6d1a477a85a21c95575bc7faece1c248bb09c6df
mcwong644 <mcwong644@gmail.com>
2024-05-13 19:41:58 +0800

    240512

--- JavaTelusko/Demo29.java
+++ JavaTelusko/Demo29.java
@@ -1,4 +1,9 @@
 public class Demo29 {
+    
+    // 08:27:11 What is Enum 
+    // 08:34:04 enum if and switch 
+    // 08:37:59 Enum Class 
+
     public static void main(String[] args) {
 
     }
---
+++ JavaTelusko/Demo30.java
@@ -0,0 +1,57 @@
+public class Demo30 {
+    // This class appears to be demonstrating the usage of Enums in Java.
+    // Based on the comments in the previous code snippets, we can infer the
+    // following:
+
+    // 08:27:11 What is Enum
+    // 08:34:04 enum if and switch
+    // 08:37:59 Enum Class
+    public static void main(String[] args) {
+        Status status = Status.PENDING;
+        System.out.println(status);
+        status = Status.INPROGRESS;
+        System.out.println(status);
+        Status st = Status.valueOf("COMPLETED");
+        System.out.println(st);
+        Status st2 = Status.COMPLETED;
+        System.out.println(st2.ordinal());
+        Status st3 = Status.values()[2];
+        System.out.println(st3);
+        System.out.println("=====ALL Statuses=========");
+        Status[] statuses = Status.values();
+        for (Status s : statuses) {
+            System.out.println(s + " " + (s.ordinal() + 1) + " " + s.name() + " " + s.toString());
+        }
+        System.out.println("=====if else======");
+        if (status == Status.INPROGRESS) {
+            System.out.println("The status is INPROGRESS");
+        } else{
+            System.out.println("The status is not INPROGRESS");
+        }
+        System.out.println("======switch======");
+        status=Status.FAILED;
+        switch (status) {
+            case PENDING:
+                System.out.println("The status is PENDING");
+                break;
+            case INPROGRESS:
+                System.out.println("The status is INPROGRESS");
+                break;
+            case COMPLETED:
+            System.out.println("The status is COMPLETED");
+            break;
+            case FAILED:
+            System.out.println("The status is FAILED");
+            break;
+            default:
+            System.out.println("The status is unknown");
+            break;
+
+        }
+    }
+
+}
+
+enum Status {
+    PENDING, INPROGRESS, COMPLETED, FAILED;
+}
