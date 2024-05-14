1054ba16d6524d4dd6d8cf0bc9cde268cbfe925a
mcwong644 <mcwong644@gmail.com>
2024-05-14 13:16:19 +0800

    240514

---
+++ JavaTelusko/Demo30.java
@@ -0,0 +1,40 @@
+public class Demo30 {
+    public static void main(String[] args) {
+        // Based on the previous code in Demo29.java, this class likely demonstrates
+        // the usage of Enums in Java.
+
+        // Declare an enum called "Status" with the following values:
+        // PENDING, INPROGRESS, COMPLETED, FAILED
+        Laptop laptop = Laptop.Macbook;
+        System.out.println(laptop.getClass().getSuperclass());
+        System.out.println(laptop + ": " + laptop.getPrice());
+        System.out.println("=====ALL Laptops=========");
+        Laptop[] laptops = Laptop.values();
+        for (Laptop l : laptops) {
+            System.out.println(l + ": " + l.getPrice());
+        }
+    }
+}
+
+enum Laptop {
+    Macbook(2000), // passing int price to the constructor
+    XPS(2200),
+    Surface, // no price, will use default constructor and default price
+    ThinkPad(1800);
+
+    private int price=500; //default price
+
+    //default constructor 
+    private Laptop() {
+    }
+
+    // constructor to initialize the price which is passed as an argument
+    private Laptop(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+}
